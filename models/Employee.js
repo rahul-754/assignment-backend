06e8292648936aeb2d71109b7424488bf9552de0
rahul-754 <161922950+rahul-754@users.noreply.github.com>
2024-07-29 00:12:56 +0530

    Update Employee.js

--- models/Employee.js
+++ models/Employee.js
@@ -1,46 +1,59 @@
-// backend/models/Employee.js
 const mongoose = require('mongoose');
 
 const employeeSchema = new mongoose.Schema({
     name: {
-        type : String,
-        required:true,
+        type: String,
+        required: true,
     },
-    email:{
-       type :  String,
-       unique: true,
-       required:true,
-    } ,
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
     mobile: {
-        type : String,
-        required:true,
+        type: String,
+        required: true,
     },
-    designation:{
-        type : String,
-        rquired:true,
-    } ,
-    gender:{
-        type : String,
-        required:true,
-    } ,
+    designation: {
+        type: String,
+        required: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+    },
     course: {
-        type : [String],
-        required:true,
+        type: [String],
+        required: true,
     },
-    image:{
-       type: String,
+    image: {
+        type: String,
     },
-  
+    employeeId: {
+        type: String,
+        unique: true,
+    },
     createdAt: { 
         type: Date,
-         default: Date.now 
+        default: Date.now 
     }
 });
 
-employeeSchema.methods.getJWTToken = function () {
-    return jwt.sign({ id: this._id }, "jghddfgxcguytytv", {
-        expiresIn: '1h'
-    });
-};
+employeeSchema.pre('save', async function(next) {
+    // Generate a unique 4-digit employee ID
+    if (this.isNew) {
+        let unique = false;
+        while (!unique) {
+            const randomId = Math.floor(1000 + Math.random()*9000).toString();
+            const existingEmployee = await mongoose.models.Employee.findOne({ employeeId: randomId });
+            if (!existingEmployee) {
+                this.employeeId = randomId;
+                unique = true;
+            }
+        }
+    }
+    next();
+});
+
 
 module.exports = mongoose.model('Employee', employeeSchema);
