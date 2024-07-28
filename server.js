390c9b7b34e6af5b783fb7893f907502cf5b5af6
rahul-754 <161922950+rahul-754@users.noreply.github.com>
2024-07-28 22:02:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(fileupload());
 
 app.use(
     cors({
-        origin:"https://assignment-frontend-qaef.onrender.com",
+        origin:"https://assignment-frontend-5wz8.onrender.com",
         credentials:true,
     })
 )
